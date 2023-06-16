cbfe38206bf78d15ff972cd944ae2413116266e6
Alfonso Martinez Bernardo <87006736+AlfonsoMartinezBern@users.noreply.github.com>
2023-06-17 00:59:01 +0200

    Update hello-openshift-for-developers.go

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,14 +9,14 @@ import (
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	
+        currentTime := time.Now()	
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
 		response = "Hello World!"
 	}
 	fmt.Fprintln(w, response)
-        currentTime := time.Now()	
 	fmt.Fprintln("YYYY-MM-DD hh:mm:ss : ", currentTime.Format("2006-01-02 15:04:05"))
-	fmt.Fprintln("Servicing an impatient beginner's request.")
+	fmt.Println("Servicing an impatient beginner's request.")
 }
 
 func listenAndServe(port string) {
